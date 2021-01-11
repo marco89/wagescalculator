66033850c0dee1c09d352bc2d9f2ecb67a51236e
marco89 <63103599+marco89@users.noreply.github.com>
2021-01-11 14:00:29 +0000

    Add files via upload
    Added function that allows user to say they didn't work on a given day.

--- hour calculator.py
+++ hour calculator.py
@@ -20,6 +20,8 @@ def tell_work():  # Relays how many hours worked
 def ask_start_time(day_name, attempts=25):  # Asks what time work was started
     for a in range(attempts):
         start = input(f'What time did you start on {day_name}?')
+        if 'na' in start:
+            return None
         validation = re.match("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$", start)
         if validation:
             return start
@@ -28,7 +30,7 @@ def ask_start_time(day_name, attempts=25):  # Asks what time work was started
         print('25 wrong attempts and you still don\'t understand that it\'s HH:MM?!')
 
 
-def ask_finish_time(day_name, attempts=25):  # Asks what time work was started
+def ask_finish_time(day_name, attempts=25):  # Asks what time work was finished
     for a in range(attempts):
         finish = input(f'What time did you finish on {day_name}?')
         validation = re.match("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$", finish)
@@ -48,71 +50,127 @@ def time_diff(a, b):  # Calculates time difference between start and finish
 
 
 start = ask_start_time('Monday')
-mon_start = datetime.datetime.strptime(start, '%H:%M')
-finish = ask_finish_time('Monday')
-mon_fin = datetime.datetime.strptime(finish, '%H:%M')
-mon_hours = time_diff(mon_start, mon_fin)
-print(mon_hours)
-outSheet.write('A2', 'Mon')
-outSheet.write('B2', (mon_hours * 24))
+if start is not None:
+    mon_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = ask_finish_time('Monday')
+    mon_fin = datetime.datetime.strptime(finish, '%H:%M')
+    mon_hours = time_diff(mon_start, mon_fin)
+    outSheet.write('A2', 'Mon')
+    outSheet.write('B2', (mon_hours * 24))
+else:
+    start = '00:00'
+    mon_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = '00:00'
+    mon_fin = datetime.datetime.strptime(finish, '%H:%M')
+    mon_hours = time_diff(mon_start, mon_fin)
+    outSheet.write('A2', 'Mon')
+    outSheet.write('B2', '-')
 
 start = ask_start_time('Tuesday')
-tue_start = datetime.datetime.strptime(start, '%H:%M')
-finish = ask_finish_time('Tuesday')
-tue_fin = datetime.datetime.strptime(finish, '%H:%M')
-tue_hours = time_diff(tue_start, tue_fin)
-print(tue_hours)
-outSheet.write('A3', 'Tue')
-outSheet.write('B3', (tue_hours * 24))
+if start is not None:
+    tue_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = ask_finish_time('Tuesday')
+    tue_fin = datetime.datetime.strptime(finish, '%H:%M')
+    tue_hours = time_diff(tue_start, tue_fin)
+    outSheet.write('A3', 'Tue')
+    outSheet.write('B3', (tue_hours * 24))
+else:
+    start = '00:00'
+    tue_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = '00:00'
+    tue_fin = datetime.datetime.strptime(finish, '%H:%M')
+    tue_hours = time_diff(tue_start, tue_fin)
+    outSheet.write('A3', 'Tue')
+    outSheet.write('B3', '-')
 
 start = ask_start_time('Wednesday')
-wed_start = datetime.datetime.strptime(start, '%H:%M')
-finish = ask_finish_time('Wednesday')
-wed_fin = datetime.datetime.strptime(finish, '%H:%M')
-wed_hours = time_diff(wed_start, wed_fin)
-print(wed_hours)
-outSheet.write('A4', 'Wed')
-outSheet.write('B4', (wed_hours * 24))
+if start is not None:
+    wed_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = ask_finish_time('Wednesday')
+    wed_fin = datetime.datetime.strptime(finish, '%H:%M')
+    wed_hours = time_diff(wed_start, wed_fin)
+    outSheet.write('A4', 'Wed')
+    outSheet.write('B4', (wed_hours * 24))
+else:
+    start = '00:00'
+    wed_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = '00:00'
+    wed_fin = datetime.datetime.strptime(finish, '%H:%M')
+    wed_hours = time_diff(wed_start, wed_fin)
+    outSheet.write('A4', 'Wed')
+    outSheet.write('B4', '-')
 
 start = ask_start_time('Thursday')
-thu_start = datetime.datetime.strptime(start, '%H:%M')
-finish = ask_finish_time('Thursday')
-thu_fin = datetime.datetime.strptime(finish, '%H:%M')
-thu_hours = time_diff(thu_start, thu_fin)
-print(thu_hours)
-outSheet.write('A5', 'Thu')
-outSheet.write('B5', (thu_hours * 24))
+if start is not None:
+    thu_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = ask_finish_time('Thursday')
+    thu_fin = datetime.datetime.strptime(finish, '%H:%M')
+    thu_hours = time_diff(thu_start, thu_fin)
+    outSheet.write('A5', 'Thu')
+    outSheet.write('B5', (thu_hours * 24))
+else:
+    start = '00:00'
+    thu_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = '00:00'
+    thu_fin = datetime.datetime.strptime(finish, '%H:%M')
+    thu_hours = time_diff(thu_start, thu_fin)
+    outSheet.write('A5', 'Thu')
+    outSheet.write('B5', '-')
 
 start = ask_start_time('Friday')
-fri_start = datetime.datetime.strptime(start, '%H:%M')
-finish = ask_finish_time('Friday')
-fri_fin = datetime.datetime.strptime(finish, '%H:%M')
-fri_hours = time_diff(fri_start, fri_fin)
-print(fri_hours)
-outSheet.write('A6', 'Fri')
-outSheet.write('B6', (fri_hours * 24))
+if start is not None:
+    fri_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = ask_finish_time('Friday')
+    fri_fin = datetime.datetime.strptime(finish, '%H:%M')
+    fri_hours = time_diff(fri_start, fri_fin)
+    outSheet.write('A6', 'Fri')
+    outSheet.write('B6', (fri_hours * 24))
+else:
+    start = '00:00'
+    fri_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = '00:00'
+    fri_fin = datetime.datetime.strptime(finish, '%H:%M')
+    fri_hours = time_diff(fri_start, fri_fin)
+    outSheet.write('A6', 'Fri')
+    outSheet.write('B6', '-')
 
 start = ask_start_time('Saturday')
-sat_start = datetime.datetime.strptime(start, '%H:%M')
-finish = ask_finish_time('Saturday')
-sat_fin = datetime.datetime.strptime(finish, '%H:%M')
-sat_hours = time_diff(sat_start, sat_fin)
-print(sat_hours)
-outSheet.write('A7', 'Sat')
-outSheet.write('B7', (sat_hours * 24))
+if start is not None:
+    sat_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = ask_finish_time('Saturday')
+    sat_fin = datetime.datetime.strptime(finish, '%H:%M')
+    sat_hours = time_diff(sat_start, sat_fin)
+    outSheet.write('A7', 'Sat')
+    outSheet.write('B7', (sat_hours * 24))
+else:
+    start = '00:00'
+    sat_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = '00:00'
+    sat_fin = datetime.datetime.strptime(finish, '%H:%M')
+    sat_hours = time_diff(sat_start, sat_fin)
+    outSheet.write('A7', 'Sat')
+    outSheet.write('B7', '-')
 
 start = ask_start_time('Sunday')
-sun_start = datetime.datetime.strptime(start, '%H:%M')
-finish = ask_finish_time('Sunday')
-sun_fin = datetime.datetime.strptime(finish, '%H:%M')
-sun_hours = time_diff(sun_start, sun_fin)
-print(sun_hours)
-outSheet.write('A8', 'Sun')
-outSheet.write('B8', (sun_hours * 24))
+if start is not None:
+    sun_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = ask_finish_time('Sunday')
+    sun_fin = datetime.datetime.strptime(finish, '%H:%M')
+    sun_hours = time_diff(sun_start, sun_fin)
+    outSheet.write('A8', 'Sun')
+    outSheet.write('B8', (sun_hours * 24))
+else:
+    start = '00:00'
+    sun_start = datetime.datetime.strptime(start, '%H:%M')
+    finish = '00:00'
+    sun_fin = datetime.datetime.strptime(finish, '%H:%M')
+    sun_hours = time_diff(sun_start, sun_fin)
+    outSheet.write('A8', 'Sun')
+    outSheet.write('B8', '-')
 
-total_hours = mon_hours + tue_hours + wed_hours + thu_hours + fri_hours + sat_hours + sun_hours
+hours = mon_hours + tue_hours + wed_hours + thu_hours + fri_hours + sat_hours + sun_hours
 
-print(total_hours)
+total_hours = hours.total_seconds() / 3600
 
 outSheet.write('A11', 'Hours worked')
 outSheet.write('B11', total_hours)
