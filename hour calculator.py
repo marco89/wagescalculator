c3ba859a3b43e9d0567938707928b1032a112bb0
marco89 <63103599+marco89@users.noreply.github.com>
2021-03-01 14:29:09 +0000

    Add files via upload
    Added a very basic tkinter-created GUI that will be expanded on and developed in the coming weeks.

--- hour calculator.py
+++ hour calculator.py
@@ -2,16 +2,39 @@ from datetime import datetime
 import datetime
 import re
 import xlsxwriter
+import tkinter as tk
+from PIL import Image, ImageTk
 
-# Creates the link to the linked excel file
-outWorkbook = xlsxwriter.Workbook('Wages.xlsx')
+root = tk.Tk()  # Creates GUI
+canvas = tk.Canvas(root, width=500, height=500)
+canvas.grid(columnspan=3, rowspan=3)
 
-# Creates a sheet within that excel file
-outSheet = outWorkbook.add_worksheet()
+logo = Image.open('titleofcalc.png')  # Adds text logo for GUI
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=2, row=0)
 
-# Names the columns
-outSheet.write('A1', 'Day')
+instructions = tk.Label(root, text='Please input your hourly wage.')  # Instructions for user on GUI
+instructions.grid(columnspan=1, column=2, row=1)
+
+entry1 = tk.Entry(root)
+canvas.create_window(200, 140, window=entry1)
+
+proceed_text = tk.StringVar()  # Creates proceed button for GUI
+proceed_button = tk.Button(root, textvariable=proceed_text, command=lambda: proceed_to_input(), bg='#858485',
+                           fg='white', height=2, width=15)
+
+outWorkbook = xlsxwriter.Workbook('Wages.xlsx')  # Creates the link to the linked Excel file
+
+outSheet = outWorkbook.add_worksheet()  # Creates a sheet within that excel file
+
+outSheet.write('A1', 'Day')  # Names the columns
 outSheet.write('B1', 'Hours')
+
+
+def proceed_to_input():  # Button functionality
+    print('Test')
 
 
 def ask_start_time(day_name, attempts=25):  # Asks what time work was started
@@ -62,6 +85,11 @@ def ask_user(days):  # Condenses ask_start_time and ask_finish_time into one fun
     return day_fin - day_start
 
 
+proceed_text.set('Next')
+proceed_button.grid(column=2, row=2)
+
+root.mainloop()  # Closes GUI instructions
+
 print('Please enter \'na\' on days you didn\'t work')
 print('')
 
@@ -88,13 +116,15 @@ hours_worked = (
 row = 1
 col = 0
 
-for day, hours in (hours_worked):
+for day, hours in hours_worked:
     outSheet.write(row, col, day)
     outSheet.write(row, col + 1, hours * 24)
     row += 1
 
 week_hours = (mon + tue + wed + thu + fri + sat + sun)
+
 total_seconds = week_hours.total_seconds()
+
 hours = ((total_seconds / 60) / 60)
 
 pay = float(hours * wage)
@@ -102,4 +132,4 @@ pay = float(hours * wage)
 outSheet.write('A10', 'Wages')
 outSheet.write('B10', pay)
 
-outWorkbook.close()
+outWorkbook.close()  # Closes and saves Excel spreadsheet
